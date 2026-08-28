1f024542554643d2cf418d0966c2b54fac4eef96
guilhermealvesribeirodossantos-afk <guilhermealvesribeirodossantos@gmail.com>
2026-08-28 20:30:07 -0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.lifeflow.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.lifeflow.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
